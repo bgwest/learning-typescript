7b561c229a3c7d7daa36f32bb032d7ec7418fc51
Benjamin G. West <23172276+bgwest@users.noreply.github.com>
2019-04-01 12:21:13 -0700

    moved constructor to top

--- src/learning-ts/static-assets.ts
+++ src/learning-ts/static-assets.ts
@@ -1,11 +1,12 @@
 class Grid {
     static origin = {x: 0, y: 0};
+    constructor (public scale: number) { }
+
     calculateDistanceFromOrigin(point: {x: number; y: number;}) {
         let xDist = (point.x - Grid.origin.x);
         let yDist = (point.y - Grid.origin.y);
         return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
     }
-    constructor (public scale: number) { }
 }
 
 module.exports = Grid;
