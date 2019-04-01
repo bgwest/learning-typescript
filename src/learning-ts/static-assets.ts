ef330aa944654dc2a26419dccc6eca1596edce2a
Benjamin G. West <23172276+bgwest@users.noreply.github.com>
2019-04-01 12:02:37 -0700

    getting typescript to run in webstorm IDE and allow module object exports without error

--- src/learning-ts/static-assets.ts
+++ src/learning-ts/static-assets.ts
@@ -8,8 +8,4 @@ class Grid {
     constructor (public scale: number) { }
 }
 
-let grid1 = new Grid(1.0);  // 1x scale
-let grid2 = new Grid(5.0);  // 5x scale
-
-console.log(grid1.calculateDistanceFromOrigin({x: 10, y: 10}));
-console.log(grid2.calculateDistanceFromOrigin({x: 10, y: 10}));
+module.exports = Grid;
